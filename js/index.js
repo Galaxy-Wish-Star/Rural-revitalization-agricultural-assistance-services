e068314336cb683ea4d00f9572f5b545e5f7ff05
liusxs <1694181697@qq.com>
2022-10-05 18:46:45 +0800

    updata
    banner轮播图的创建与完善

--- js/index.js
+++ js/index.js
@@ -142,3 +142,93 @@ caigou.onclick = function () {
 	console.log(this);
 };
 
+// 以下为轮播图js
+var banner=document.querySelector('#banner-1');
+var bannerImg=document.querySelectorAll('.img-list img');
+var bannerlist=document.querySelectorAll('.pointer a');
+var index=0;
+//  (1.鼠标不在轮播图区域的时候 轮播图正常播放)
+var t=setInterval(function(){
+    if(index==2){
+        index=0;
+    }else{
+        index++;
+    }
+    for(var n=0;n<=2;n++){
+        bannerImg[n].className='';
+    }
+    bannerImg[index].className='show';
+    for(var n=0;n<=2;n++){
+        bannerlist[n].className='';
+    }
+    bannerlist[index].className='active';
+}, 2000)
+//  (2.鼠标移入轮播图区域 轮播图播放暂停)
+banner.addEventListener('mouseenter',function(){
+    clearInterval(t);
+})
+//  (3.鼠标移出轮播图区域 轮播图恢复播放)
+banner.addEventListener('mouseleave',function(){
+    t=setInterval(function(){
+        if(index==2){
+        index=0;
+    }else{
+        index++;
+    }
+    for(var n=0;n<=2;n++){
+        bannerImg[n].className='';
+    }
+    bannerImg[index].className='show';
+    for(var n=0;n<=2;n++){
+        bannerlist[n].className='';
+    }
+    bannerlist[index].className='active';
+    },2000)
+})
+var bannerLeft=document.querySelector('#bannerLeft');
+var bannerRight=document.querySelector('#bannerRight');
+bannerLeft.addEventListener('click',function(){
+    if(index==0){
+        index=2;
+    }else{
+        index--;
+    }
+    for(var n=0;n<=2;n++){
+        bannerImg[n].className='';
+    }
+    bannerImg[index].className='show';
+    for(var n=0;n<=2;n++){
+        bannerlist[n].className='';
+    }
+    bannerlist[index].className='active';
+})
+bannerRight.addEventListener('click',function(){
+    if(index==2){
+        index=0;
+    }else{
+        index++;
+    }
+    for(var n=0;n<=2;n++){
+        bannerImg[n].className='';
+    }
+    bannerImg[index].className='show';
+    for(var n=0;n<=2;n++){
+        bannerlist[n].className='';
+    }
+    bannerlist[index].className='active';
+})
+// 鼠标移入左下方导航（小圆点）切换到对应的图片
+for(var n=0;n<=2;n++){
+    bannerlist[n].index=n;
+    bannerlist[n].addEventListener('mouseenter',function(){
+        for(var n=0;n<=2;n++){
+            bannerImg[n].className='';
+        }
+        index=this.index;
+        for(var n=0;n<=2;n++){
+            bannerlist[n].className='';
+        }
+        bannerImg[index].className='show';
+        bannerlist[index].className='active';
+    })
+}
