4435fcff1383258d472912da749c4ed63b72e94f
mason369 <maosenyang00@gmail.com>
2022-10-09 22:27:04 +0800

    update mobile

--- js/mobile.js
+++ js/mobile.js
@@ -1,45 +1,4 @@
-// // 以下为轮播图js
-// var banner=document.querySelector('#banneres-1');
-// var bannerImg=document.querySelectorAll('.banneres img');
-// var index=0;
-// //  (1.鼠标不在轮播图区域的时候 轮播图正常播放)
-// var t=setInterval(function(){
-//     if(index==2){
-//         index=0;
-//     }else{
-//         index++;
-//     }
-//     for(var n=0;n<=2;n++){
-//         bannerImg[n].className='';
-//     }
-//     bannerImg[index].className='show';
-//     for(var n=0;n<=2;n++){
-//         bannerlist[n].className='';
-//     }
-//     bannerlist[index].className='active';
-// }, 2000)
-// //  (2.鼠标移入轮播图区域 轮播图播放暂停)
-// banner.addEventListener('mouseenter',function(){
-//     clearInterval(t);
-// })
-// //  (3.鼠标移出轮播图区域 轮播图恢复播放)
-// banner.addEventListener('mouseleave',function(){
-//     t=setInterval(function(){
-//         if(index==2){
-//         index=0;
-//     }else{
-//         index++;
-//     }
-//     for(var n=0;n<=2;n++){
-//         bannerImg[n].className='';
-//     }
-//     bannerImg[index].className='show';
-//     for(var n=0;n<=2;n++){
-//         bannerlist[n].className='';
-//     }
-//     bannerlist[index].className='active';
-//     },2000)
-// })
+
 window.addEventListener("load", function () {
 	var advertise = document.querySelector(".header-banner-head");
 	var topbar = document.querySelector(".topbar");
@@ -111,6 +70,8 @@ window.addEventListener("load", function () {
 	footer_bar[1].addEventListener("click", () => {
 		shop_car.style.display = "block";
 		zhuye.style.display = "none";
-		
+		for (var i = 0; i < 3; i++) {
+			topbar_ele[i].style.display = "none";
+		}
 	});
 });
