e0fdf82dc810d32474c8218923b0dcbd5bc35c45
小小怪 <45096653@qq.com>
2022-10-14 19:04:59 +0800

    update
    修复轮播图bug

--- js/mobile.js
+++ js/mobile.js
@@ -126,21 +126,21 @@ window.onload = function () {
 	});
 
 	var swiper = new Swiper(".mySwiper", {
-		spaceBetween: 30,
-		centeredSlides: true,
-		autoplay: {
-			delay: 1700,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
-		},
-		navigation: {
-			nextEl: ".swiper-button-next",
-			prevEl: ".swiper-button-prev",
-		},
-	});
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
 	// 秒杀
 	var hours = document.querySelector(".j_sk_h");
