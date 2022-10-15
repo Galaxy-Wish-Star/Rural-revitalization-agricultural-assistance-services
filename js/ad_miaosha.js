20fb113839e707e01939979799080574d7ca8a27
mason369 <maosenyang00@gmail.com>
2022-10-15 17:01:10 +0800

    update
    更新秒杀动画

--- js/ad_miaosha.js
+++ js/ad_miaosha.js
@@ -1,39 +1,48 @@
-
 //秒杀广告
 
 // 获取box的ID
 var box = document.getElementById("miaosha-box");
-
+var i = document.getElementById("arrow-ad-animeat");
 // 设置进入动画
-setTimeout(function() {
+setTimeout(function () {
 	animation(box, {
-		"width": 369
-	})
+		width: 369,
+	});
 }, 1500);
 
 // 设置移出动画
-setTimeout(function() {
+setTimeout(function () {
 	animation(box, {
-		"width": 35
-	})
+		width: 35,
+	});
 }, 6000);
-box.addEventListener('mouseenter', function(){
-	setTimeout(function() {
+box.addEventListener("mouseenter", function () {
+	setTimeout(function () {
 		animation(box, {
-			"width": 369
-		})
+			width: 369,
+		});
 	}, 0);
-})
-box.addEventListener('mouseleave', function(){
-	setTimeout(function() {
+	var deg=180
+	//长度加上单位deg
+	var d = deg + "deg";
+	//拼接控制旋转的方法
+	i.style.transform = "rotate(" + d + ")";
+});
+box.addEventListener("mouseleave", function () {
+	setTimeout(function () {
 		animation(box, {
-			"width": 35
-		})
+			width: 35,
+		});
 	}, 0);
-})
+	var deg=0
+	//长度加上单位deg
+	var d = deg + "deg";
+	//拼接控制旋转的方法
+	i.style.transform = "rotate(" + d + ")";
+});
 /*获取任意元素样式值*/
 function getElementStyleValue(element, attr) {
-	if(window.getComputedStyle) {
+	if (window.getComputedStyle) {
 		return window.getComputedStyle(element, null)[attr];
 	} else {
 		return element.currentStyle[attr];
@@ -46,9 +55,9 @@ function setOpacityValue(element, value) {
 	value = value < 0 ? 0 : value;
 	value = value > 1 ? 1 : value;
 	// 判断浏览器是否支持opacity
-	if(document.all) {
+	if (document.all) {
 		// 设置IE浏览器透明度值
-		element.style.filter = "alpha(opacity=" + (value * 100) + ")";
+		element.style.filter = "alpha(opacity=" + value * 100 + ")";
 	} else {
 		// 设置非IE浏览器透明度
 		element.style.opacity = value;
@@ -58,14 +67,22 @@ function setOpacityValue(element, value) {
 /*获取页面向上或者向左卷曲出去的距离的值*/
 function getScroll() {
 	return {
-		left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0,
-		top: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+		left:
+			window.pageXOffset ||
+			document.documentElement.scrollLeft ||
+			document.body.scrollLeft ||
+			0,
+		top:
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop ||
+			0,
 	};
 }
 
 /*获取任意标签的内容*/
 function getInnerText(element) {
-	if(typeof element.textContent == "undefined") {
+	if (typeof element.textContent == "undefined") {
 		return element.innerText;
 	} else {
 		return element.textContent;
@@ -74,18 +91,18 @@ function getInnerText(element) {
 
 /*设置任意标签的内容*/
 function setInnerText(element, text) {
-	if(typeof element.textContent == "undefined") {
-		return element.innerText = text;
+	if (typeof element.textContent == "undefined") {
+		return (element.innerText = text);
 	} else {
-		return element.textContent = text;
+		return (element.textContent = text);
 	}
 }
 
 /*为任意元素绑定事件*/
 function addEventListener(element, type, fn) {
-	if(element.addEventListener) {
+	if (element.addEventListener) {
 		element.addEventListener(type, fn, false);
-	} else if(element.attachEvent) {
+	} else if (element.attachEvent) {
 		element.attachEvent("on" + type, fn);
 	} else {
 		element["on" + type] = fn;
@@ -94,9 +111,9 @@ function addEventListener(element, type, fn) {
 
 /*为任意元素解绑事件*/
 function removeEventListener(element, type, fnName) {
-	if(element.removeEventListener) {
+	if (element.removeEventListener) {
 		element.removeEventListener(type, fnName, false);
-	} else if(element.detachEvent) {
+	} else if (element.detachEvent) {
 		element.detachEvent("on" + type, fnName);
 	} else {
 		element["on" + type] = null;
@@ -105,11 +122,11 @@ function removeEventListener(element, type, fnName) {
 
 /*获取任意一个父级元素的第一个子元素*/
 function getfirstElementChild(element) {
-	if(element.firstElementChild) {
+	if (element.firstElementChild) {
 		return element.firstElementChild;
 	} else {
 		var node = element.firstChild;
-		while(node && node.nodeType != 1) {
+		while (node && node.nodeType != 1) {
 			node = node.nextSibling;
 		}
 		return node;
@@ -118,11 +135,11 @@ function getfirstElementChild(element) {
 
 /*获取任意一个父级元素的最后一个子元素*/
 function getLastElementChild(element) {
-	if(element.lastElementChild) {
+	if (element.lastElementChild) {
 		return element.lastElementChild;
 	} else {
 		var node = element.lastChild;
-		while(node && node.nodeType != 1) {
+		while (node && node.nodeType != 1) {
 			node = node.previousSibling;
 		}
 		return node;
@@ -131,11 +148,11 @@ function getLastElementChild(element) {
 
 /*获取任意一个子元素的前一个兄弟元素*/
 function getPreviousElementSibling(element) {
-	if(element.previousElementSibling) {
+	if (element.previousElementSibling) {
 		return element.previousElementSibling;
 	} else {
 		var node = element.previousSibling;
-		while(node && node.nodeType != 1) {
+		while (node && node.nodeType != 1) {
 			node = node.previousSibling;
 		}
 		return node;
@@ -144,11 +161,11 @@ function getPreviousElementSibling(element) {
 
 /*获取任意一个子元素的后一个兄弟元素*/
 function getNextElementSibling(element) {
-	if(element.nextElementSibling) {
+	if (element.nextElementSibling) {
 		return element.nextElementSibling;
 	} else {
 		var node = element.nextSibling;
-		while(node && node.nodeType != 1) {
+		while (node && node.nodeType != 1) {
 			node = node.nextSibling;
 		}
 		return node;
@@ -160,13 +177,13 @@ function animation(element, json, fn) {
 	// 清理定时器
 	clearInterval(element.timeId);
 	// 设置定时器
-	element.timeId = setInterval(function() {
+	element.timeId = setInterval(function () {
 		// 标记位，默认所有动作都已完成
 		var flag = true;
 		// 循环遍历所有属性值
-		for(var attr in json) {
+		for (var attr in json) {
 			// 判断是否是透明度属性
-			if(attr == "opacity") {
+			if (attr == "opacity") {
 				// 获取当前元素的当前透明度值并且扩大100倍
 				var current = getElementStyleValue(element, attr) * 100;
 				// 设置终点透明度值并且扩大100倍
@@ -178,9 +195,9 @@ function animation(element, json, fn) {
 				// 移动后的步数值
 				current += step;
 				// 缩小100倍并设置移动后元素的透明度值
-				element.style[attr] = (current / 100);
+				element.style[attr] = current / 100;
 				// 判断是否是层级属性
-			} else if(attr == "zIndex") {
+			} else if (attr == "zIndex") {
 				// 直接设置元素的层级
 				element.style[attr] = json[attr];
 				// 普通属性
@@ -199,18 +216,17 @@ function animation(element, json, fn) {
 				element.style[attr] = current + "px";
 			}
 			// 判断是否到达目标
-			if(current != target) {
+			if (current != target) {
 				flag = false;
 			}
 		}
 		// 如果全部动作都已完成，则清理定时器
-		if(flag) {
+		if (flag) {
 			clearInterval(element.timeId);
 			// 当动画完成，判断用户是否传入了回调函数
-			if(fn) {
+			if (fn) {
 				fn();
 			}
 		}
-
 	}, 20);
 }
