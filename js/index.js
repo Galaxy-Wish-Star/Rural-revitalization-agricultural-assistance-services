a35232a3b24db0aca7d3e787654167155575ca4f
mason369 <maosenyang00@gmail.com>
2022-10-18 16:14:06 +0800

    update

--- js/index.js
+++ js/index.js
@@ -584,4 +584,20 @@ $(function(){
 		transtion(180*4, fa_sync);
 		}
 	};
+	var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 })
