0cb3955b939d8aa544431336ed29aa383d8fec97
mason369 <maosenyang00@gmail.com>
2022-10-20 12:58:02 +0800

    update data to img
    更新数据可视化

--- js/index.js
+++ js/index.js
@@ -697,7 +697,7 @@ $(function () {
 	}
 });
 
-//搜索关键词联想
+//首页搜索关键词联想
 var input = document.querySelector(".search-input");
 var oUl = document.getElementById("ul");
 input.onkeyup = function () {
@@ -727,8 +727,125 @@ function aa(data) {
 		});
 		oUl.innerHTML = str;
 	}
-	input.onblur= function () {
+	input.onblur = function () {
 		oUl.style.display = "none";
-		input.value = "请输入内容"
+		input.value = "请输入内容";
 	};
 }
+
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById("main"));
+
+// 指定图表的配置项和数据
+var option = {
+	title: {
+		text: "湖北省滞销监控",
+	},
+	tooltip: {},
+	legend: {
+		data: ["滞销量/万斤"],
+	},
+	xAxis: {
+		data: ["十堰", "襄阳", "荆门", "黄石", "黄冈", "宜昌"],
+	},
+	yAxis: {},
+	series: [
+		{
+			name: "滞销量/万斤",
+			type: "bar",
+			data: [5, 20, 36, 10, 10, 20],
+		},
+	],
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
+
+
+
+
+//动画数据
+var myChart = echarts.init(document.getElementById("main-2"));
+setTimeout(function () {
+	option = {
+	  legend: {},
+	  tooltip: {
+		trigger: 'axis',
+		showContent: false
+	  },
+	  dataset: {
+		source: [
+		  ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
+		  ['河南省', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+		  ['湖北省', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+		  ['安徽省', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+		  ['重庆', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+		]
+	  },
+	  xAxis: { type: 'category' },
+	  yAxis: { gridIndex: 0 },
+	  grid: { top: '55%' },
+	  series: [
+		{
+		  type: 'line',
+		  smooth: true,
+		  seriesLayoutBy: 'row',
+		  emphasis: { focus: 'series' }
+		},
+		{
+		  type: 'line',
+		  smooth: true,
+		  seriesLayoutBy: 'row',
+		  emphasis: { focus: 'series' }
+		},
+		{
+		  type: 'line',
+		  smooth: true,
+		  seriesLayoutBy: 'row',
+		  emphasis: { focus: 'series' }
+		},
+		{
+		  type: 'line',
+		  smooth: true,
+		  seriesLayoutBy: 'row',
+		  emphasis: { focus: 'series' }
+		},
+		{
+		  type: 'pie',
+		  id: 'pie',
+		  radius: '30%',
+		  center: ['50%', '25%'],
+		  emphasis: {
+			focus: 'self'
+		  },
+		  label: {
+			formatter: '{b}: {@2012} ({d}%)'
+		  },
+		  encode: {
+			itemName: 'product',
+			value: '2012',
+			tooltip: '2012'
+		  }
+		}
+	  ]
+	};
+	myChart.on('updateAxisPointer', function (event) {
+	  const xAxisInfo = event.axesInfo[0];
+	  if (xAxisInfo) {
+		const dimension = xAxisInfo.value + 1;
+		myChart.setOption({
+		  series: {
+			id: 'pie',
+			label: {
+			  formatter: '{b}: {@[' + dimension + ']} ({d}%)'
+			},
+			encode: {
+			  value: dimension,
+			  tooltip: dimension
+			}
+		  }
+		});
+	  }
+	});
+	myChart.setOption(option);
+  });
