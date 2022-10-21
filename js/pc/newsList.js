06309cc8f1c8501ff4191c25a73f0abae0ec4f7b
mason369 <maosenyang00@gmail.com>
2022-10-22 00:04:12 +0800

    mods

--- js/pc/newsList.js
+++ js/pc/newsList.js
@@ -44,4 +44,21 @@ $(function () {
         // 3. 其余的兄弟去掉背景颜色 隐式迭代
         $(this).siblings('a').css({"background":"","color":"#434343"});
 	});
+	$(".up-page,.down-page").click(function () {
+		// $(this).css({ "background-color": "#fff", color: "#434343" });
+		// $(this)
+		// 	.siblings("button")
+		// 	.css({ "background-color": "#434343", color: "#fff" });
+   
+		$.get(
+			"https://www.mxnzp.com/api/news/list?typeId=511&page=4&app_id=qhoalrrshsilrpkg&app_secret=Rm9qSElZZDJzaXZ5UHNuMGZtWThzZz09",
+			function (res) {
+				if (res.code !== 1) {
+					return alert("数据请求失败");
+				}
+				var htmlList = template("tpl-news2", res);
+				$("#tpl-news1").html(htmlList);
+			},
+		);
+	});
 });
