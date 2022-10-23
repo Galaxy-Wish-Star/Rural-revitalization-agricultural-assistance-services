52ee588a7247af7f6b982a5f019e5a094c51b278
mason369 <maosenyang00@gmail.com>
2022-10-23 13:19:55 +0800

    Update newsList.js

--- js/pc/newsList.js
+++ js/pc/newsList.js
@@ -11,34 +11,47 @@ $(function () {
 					return alert("数据请求失败");
 				}
 				for (var i = 0; i < 8; i++) {
+					if(res.data.data.length>4){
 					//删除多余值
 					res.data.data.shift();
+					}
 				}
-
 				console.log(res.data.data.length);
 				console.log(res.data.data);
-				console.log(res.data.data.length !== 4);
-				console.log(res.data.data[0].imgList === null);
-				console.log(res.data.data.length !== 4 || res.data.data[0].imgList === null)
-				if ((res.data.data.length !== 4 || res.data.data[0].imgList === null)){
-					axios
-					.get(
-						"https://www.mxnzp.com/api/news/list?typeId=511&page=1&app_id=qhoalrrshsilrpkg&app_secret=Rm9qSElZZDJzaXZ5UHNuMGZtWThzZz09",
-					)
-					.then(function (res) {
-						if (res.data.code !== 1 || res.code == 102) {
-							return alert("数据请求失败");
-						}
-						for (var i = 0; i < 8; i++) {
-							//删除多余值
-							res.data.data.shift();
-						}
-					});
-					console.log(res.data.data.length);
-					console.log(res.data.data);
-					console.log(res.data.data.length !== 4);
-					console.log(res.data.data[0].imgList === null);
+				console.log(res.data.data[0].imgList[0] = "../img/zjsp/云南雪莲果.jpg");
+				console.log(
+					res.data.data.length !== 4 || res.data.data[0].imgList === null,
+				);
+				if (res.data.data[0].imgList === null) {
+					for (var i = 0; i < 4; i++) {
+						res.data.data[i].imgList[0] = "../img/zjsp/云南雪莲果.jpg"
+						console.log(imgList[i])
+					}
 				}
+				// for(;res.data.data.length !== 4 || res.data.data[0].imgList === null;){
+				// 	setTimeout(() => {
+				// 		axios
+				// 	.get(
+				// 		"https://www.mxnzp.com/api/news/list?typeId=511&page=1&app_id=qhoalrrshsilrpkg&app_secret=Rm9qSElZZDJzaXZ5UHNuMGZtWThzZz09",
+				// 	)
+				// 	.then(function (res) {
+				// 		if (res.data.code !== 1 || res.code == 102) {
+				// 			return alert("数据请求失败");
+				// 		}
+				// 		for (var i = 0; i < 8; i++) {
+				// 			//删除多余值
+				// 			res.data.data.shift();
+				// 		}
+				// 	});
+				// 	}, 2000);
+				// 	if((res.data.data.length !== 4 || res.data.data[0].imgList === null)==false){
+				// 		return false
+				// 	}
+				// 	console.log(res.data.data.length);
+				// 	console.log(res.data.data);
+				// 	console.log(res.data.data.length !== 4);
+				// 	console.log(res.data.data[0].imgList === null);
+				// }
 				var htmlList = template("tpl-news2", res.data);
 				$("#tpl-news1").html(htmlList);
 			});
